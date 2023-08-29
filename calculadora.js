52132c31da8b1f594c739c71ddedd14f753587e4
mariana citadin <marianacitadin@gmail.com>
2023-08-29 15:16:39 -0300

    versão 1

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
+var subtracao = require("./modulos/subtracao");
+var multiplicacao = require("./modulos/multiplicacao");
+var divisao = require("./modulos/divisao");
 var media = require("./modulos/media");
 
-console.log(somar(20,90));
-console.log(media(20,90));
+console.log(somar(12,84));
+console.log(subtracao(25,34));
+console.log(multiplicacao(23,90));
+console.log(divisao(20,10));
+console.log(media(22,90));
